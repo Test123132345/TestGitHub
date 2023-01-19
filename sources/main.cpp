350768fa404d9fd38921785cf1e41356d72c21d8
Kohut Yaroslav <kogutyaroslav3947@gmail.com>
2023-01-19 12:28:30 +0200

    rm everything

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-int sum(int a, int b){
-    return a + b;
-}
+
 int main() {
-    std::cout << "Hello World!\nInput rand num";
-    int num;
-    std::cin >> num;
-    if (num == 1) {
-        std::cout << "HUI!";
-    }
-    else {
-        std::cout << "test";
-    }
-    std::cout << "99";
+    
 
     return 0;
 }
